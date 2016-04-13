0188664f3cbee0acd68f78c99c25c881017f4cf5
Quoc Bui <bui.vquoc@gmail.com>
2016-04-13 17:50:33 +0200

    change sass structure > add setup grid neat & bourbon > ready to start w components

---
+++ config.rb
@@ -0,0 +1,48 @@
+require 'slim'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# External pipeline for gulp assets
+activate :external_pipeline,
+  name: :gulp,
+  command: build? ? './node_modules/gulp/bin/gulp.js default' : './node_modules/gulp/bin/gulp.js watch',
+  source: "./flekstub",
+  latency: 1
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify html on build
+  activate :minify_html
+
+  # Append a hash to asset urls (make sure to use the url helpers)
+  #activate :asset_hash
+end
